96d03a3edba5c022ec69f40df48876d4d8e9e90f
Jason Puglisi <jason@puglisi.im>
2014-05-27 08:06:17 -0400

    Moved project to CloudPebble and fixed font names

--- src/slammin_watchface.c
+++ src/slammin_watchface.c
@@ -133,7 +133,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(header_text_layer, GColorClear);
   text_layer_set_text_color(header_text_layer, GColorBlack);
   text_layer_set_text_alignment(header_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(header_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_18_BOLD)));
+  text_layer_set_font(header_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_BOLD_18)));
   text_layer_set_text(header_text_layer, header_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) header_text_layer);
 
@@ -142,7 +142,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(intro_text_layer, GColorClear);
   text_layer_set_text_color(intro_text_layer, GColorBlack);
   text_layer_set_text_alignment(intro_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(intro_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_14)));
+  text_layer_set_font(intro_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_14)));
   text_layer_set_text(intro_text_layer, intro_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) intro_text_layer);
 
@@ -151,7 +151,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(hour_text_layer, GColorClear);
   text_layer_set_text_color(hour_text_layer, GColorBlack);
   text_layer_set_text_alignment(hour_text_layer, GTextAlignmentRight);
-  text_layer_set_font(hour_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_42)));
+  text_layer_set_font(hour_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_42)));
   text_layer_set_text(hour_text_layer, hour_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) hour_text_layer);
 
@@ -160,7 +160,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(colon_text_layer, GColorClear);
   text_layer_set_text_color(colon_text_layer, GColorBlack);
   text_layer_set_text_alignment(colon_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(colon_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_42)));
+  text_layer_set_font(colon_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_42)));
   text_layer_set_text(colon_text_layer, colon_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) colon_text_layer);
 
@@ -168,7 +168,7 @@ void window_load(Window *window) {
   minute_text_layer = text_layer_create(GRect(78, 54, 66, 42));
   text_layer_set_background_color(minute_text_layer, GColorClear);
   text_layer_set_text_color(minute_text_layer, GColorBlack);
-  text_layer_set_font(minute_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_42)));
+  text_layer_set_font(minute_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_42)));
   text_layer_set_text(minute_text_layer, minute_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) minute_text_layer);
 
@@ -177,7 +177,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(outro_text_layer, GColorClear);
   text_layer_set_text_color(outro_text_layer, GColorBlack);
   text_layer_set_text_alignment(outro_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(outro_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_14)));
+  text_layer_set_font(outro_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_14)));
   text_layer_set_text(outro_text_layer, outro_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) outro_text_layer);
 
@@ -186,7 +186,7 @@ void window_load(Window *window) {
   text_layer_set_background_color(footer_text_layer, GColorClear);
   text_layer_set_text_color(footer_text_layer, GColorBlack);
   text_layer_set_text_alignment(footer_text_layer, GTextAlignmentCenter);
-  text_layer_set_font(footer_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_KEY_UBUNTU_18_BOLD)));
+  text_layer_set_font(footer_text_layer, fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_UBUNTU_BOLD_18)));
   text_layer_set_text(footer_text_layer, footer_buffer);
   layer_add_child(window_get_root_layer(window), (Layer*) footer_text_layer);
 
--- wscript
+++ wscript
@@ -5,6 +5,12 @@
 # Feel free to customize this to your needs.
 #
 
+try:
+    from sh import CommandNotFound, jshint, cat, ErrorReturnCode_2
+    hint = jshint
+except (ImportError, CommandNotFound):
+    hint = None
+
 top = '.'
 out = 'build'
 
@@ -13,8 +19,22 @@ def options(ctx):
 
 def configure(ctx):
     ctx.load('pebble_sdk')
+    global hint
+    if hint is not None:
+        hint = hint.bake(['--config', 'pebble-jshintrc'])
 
 def build(ctx):
+    if False and hint is not None:
+        try:
+            hint([node.abspath() for node in ctx.path.ant_glob("src/**/*.js")], _tty_out=False) # no tty because there are none in the cloudpebble sandbox.
+        except ErrorReturnCode_2 as e:
+            ctx.fatal("\nJavaScript linting failed (you can disable this in Project Settings):\n" + e.stdout)
+
+    # Concatenate all our JS files (but not recursively).
+
+    ctx.path.make_node('src/js/').mkdir()
+    ctx.exec_command(['cat'] + [node.abspath() for node in ctx.path.ant_glob("src/*.js")], stdout=open('src/js/pebble-js-app.js', 'a'))
+
     ctx.load('pebble_sdk')
 
     ctx.pbl_program(source=ctx.path.ant_glob('src/**/*.c'),
@@ -22,3 +42,4 @@ def build(ctx):
 
     ctx.pbl_bundle(elf='pebble-app.elf',
                    js=ctx.path.ant_glob('src/js/**/*.js'))
+
